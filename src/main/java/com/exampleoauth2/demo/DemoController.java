c53afbd35a6e7678658b97dd87072fb24e5c0f15
Srilalitha S <srilalit@thoughtworks.com>
2020-11-09 20:36:54 +0530

    Add endpoint to show logged in user profile

--- src/main/java/com/exampleoauth2/demo/DemoController.java
+++ src/main/java/com/exampleoauth2/demo/DemoController.java
@@ -1,5 +1,6 @@
 package com.exampleoauth2.demo;
 
+import java.security.Principal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
   @RequestMapping(value = "/", method = RequestMethod.GET)
-    private String home() {
-      return "Welcome home!";
-    }
+  private String home() {
+    return "Welcome home!";
+  }
+
+  @RequestMapping(value = "/profile", method = RequestMethod.GET)
+  private Principal user(Principal principal) {
+    return principal;
+  }
 }
